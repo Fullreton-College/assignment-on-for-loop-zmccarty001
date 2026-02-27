32614ffcf2a3ef83e858e36b0a8663ccc3386448
Turner, Zachariah <z00114761@gmail.com>
2026-02-26 18:25:46 -0800

    coding changes complete

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -1,10 +1,26 @@
-// Chapter 5, Programming Challenge 17: Sales Bar Chart
+// I submitted the sales on the other assignment, this one is the two integers assignment
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
+    int first_Int, second_Int;
+    int increment = 5;
 
-return 0;
+    cout<<"Enter first integer: \n";
+    cin>>first_Int;
+    cout<<"Enter second integer: \n";
+    cin>>second_Int;
+
+    cout<<first_Int<<" ";
+
+    if (first_Int < second_Int) {
+    for (int count = 0; first_Int <= second_Int; count++) {
+        cout<< (first_Int += increment)<<" ";
+    }
+
+    
+    return 0;
+}
 }
