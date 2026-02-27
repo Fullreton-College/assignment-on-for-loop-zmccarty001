7a3a5ff13613d880a7a5f94a2db2518a0fec8b22
Turner, Zachariah <z00114761@gmail.com>
2026-02-26 17:26:13 -0800

    coding changes made

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
 
-   /* Type your code here. */
+
+   int sales_1, sales_2, sales_3, sales_4, sales_5;
+
+   cout<<"Enter today's sale for Store 1: \n";
+   cin>>sales_1;
+   cout<<"Enter today's sale for Store 2: \n";
+   cin>>sales_2;
+   cout<<"Enter today's sale for Store 3: \n";
+   cin>>sales_3;
+   cout<<"Enter today's sale for Store 4: \n";
+   cin>>sales_4;
+   cout<<"Enter today's sale for Store 5: \n";
+   cin>>sales_5;
+   cout << "Sales Bar Chart:\n";
+   cout << "Each * = $100\n";
+
+   cout << "Store 1: ";
+    for (int count = 0; count < (sales_1/100); count++) 
+    {
+      cout<<"*";
+      } 
+      cout<< endl;
+   cout << "Store 2: "; 
+   for (int count = 0; count < (sales_2/100); count++) {
+      cout<<"*";
+   } 
+   cout << endl;
+   cout << "Store 3: ";
+   for (int count = 0; count < (sales_3/100); count++) {
+      cout<<"*";
+   }
+   cout << endl;
+   cout << "Store 4: ";
+   for (int count = 0; count < (sales_4/100); count++) {
+      cout<<"*";
+   } 
+   cout << endl;
+   cout << "Store 5: ";
+   for (int count = 0; count < (sales_5/100); count++ ) {
+      cout<<"*";
+   }
+   cout << endl;
+   
+
+
+
 
    return 0;
 }
